7c9f5abe7c099a8575cec0f8181838a88afdaf1c
madhavmanohars <madhav.ms@inapp.com>
2018-08-17 11:25:49 +0530

    added set length and breadthand tested instanceof

--- 15-08-18/FirstJava/com.inapp.Firstjava.test/TestSquare.java
+++ 15-08-18/FirstJava/com.inapp.Firstjava.test/TestSquare.java
@@ -11,7 +11,18 @@ public class TestSquare{
 		for(int i=0;i<arrayLength;i++) {
 		System.out.println(squareArr[i].getArea());
 		
-	}
+	}   Square square1=new Square();
+		System.out.println("area of square : "+square1.getArea());
+
+		System.out.println("Is square : "+square1.isSquare());
+		square1.setLength(10);
+
+		System.out.println("Is square : "+square1.isSquare());
+		System.out.println("area of square : "+square1.getArea());
+		square1.setBreadth(10);
+		System.out.println("area of square : "+square1.getArea());
+		System.out.println("Is square : "+square1.isSquare());
+		System.out.println("Is ** square : "+(square1 instanceof Rectangle));
 	}
 }
 
