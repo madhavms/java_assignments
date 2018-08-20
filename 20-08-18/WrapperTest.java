1153c9ee02f2db609416ede478949058c36e0729
madhavmanohars <madhav.ms@inapp.com>
2018-08-20 09:31:44 +0530

    Wrapper Class

---
+++ 20-08-18/WrapperTest.java
@@ -0,0 +1,10 @@
+package com.inapp.Firstjava.test;
+
+public class WrapperTest {
+public static void main(String[] args) {
+	Integer x=10; //boxing
+	x=x+5;        //unboxing
+	System.out.println(x);
+}
+}
+
