4ad282385892c92b96a3f1e30f68f3545bed19d4
madhavmanohars <madhav.ms@inapp.com>
2018-08-20 16:42:16 +0530

    Test square and rectangle added

--- 20-08-18/Interface_Program/com.inapp.Firstjava.test/PolygonCalculatorTest.java
+++ 20-08-18/Interface_Program/com.inapp.Firstjava.test/PolygonCalculatorTest.java
@@ -1,13 +1,26 @@
 package com.inapp.Firstjava.test;
 import com.inapp.Firstjava.learn.RegularPolygon;
-
+import com.inapp.Firstjava.learn.Square;
+import com.inapp.Firstjava.learn.Rectangle;
 import com.inapp.Firstjava.learn.inter_face.PolygonAreaCalculatorService;
 
 public class PolygonCalculatorTest {
 
 	public static void main(String[] args) {
 		PolygonCalculatorTest calculatorTest = new PolygonCalculatorTest();
-	    calculatorTest.testPolygon();
+		calculatorTest.testSquare();
+		calculatorTest.testRectangle();
+		calculatorTest.testPolygon();
+	}
+	public void testSquare() {
+
+		PolygonAreaCalculatorService calculatorservice = new Square(3);
+		System.out.println("Area of square:"+calculatorservice.calculateArea());
+	}
+	public void testRectangle() {
+
+		PolygonAreaCalculatorService calculatorservice = new Rectangle(3,7);
+		System.out.println("Area of rectangle:"+calculatorservice.calculateArea());
 	}
 	public void testPolygon() {
 		PolygonAreaCalculatorService calculatorservice = new RegularPolygon(3,8); 
