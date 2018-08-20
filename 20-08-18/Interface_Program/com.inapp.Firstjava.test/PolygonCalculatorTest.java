97f580f88d4a48f2e27c45ae377c065381a67dd1
Madhav Manohar S <madhav.ms@inapp.com>
2018-08-20 22:28:33 +0530

    Updated Code

--- 20-08-18/Interface_Program/com.inapp.Firstjava.test/PolygonCalculatorTest.java
+++ 20-08-18/Interface_Program/com.inapp.Firstjava.test/PolygonCalculatorTest.java
@@ -3,7 +3,7 @@ import com.inapp.Firstjava.learn.RegularPolygon;
 import com.inapp.Firstjava.learn.Square;
 import com.inapp.Firstjava.learn.Rectangle;
 import com.inapp.Firstjava.learn.inter_face.PolygonAreaCalculatorService;
-
+import com.inapp.Firstjava.learn.inter_face.PolygonPerimeterCalculatorService;;
 public class PolygonCalculatorTest {
 
 	public static void main(String[] args) {
@@ -15,16 +15,22 @@ public class PolygonCalculatorTest {
 	public void testSquare() {
 
 		PolygonAreaCalculatorService calculatorservice = new Square(3);
-		System.out.println("Area of square:"+calculatorservice.calculateArea());
+		PolygonPerimeterCalculatorService pericalculatorservice = new Square(3);
+		System.out.println("\nArea of square:"+calculatorservice.calculateArea());
+		System.out.println("\nPerimeter of square:"+pericalculatorservice.caculatePerimeter());
 	}
 	public void testRectangle() {
 
 		PolygonAreaCalculatorService calculatorservice = new Rectangle(3,7);
-		System.out.println("Area of rectangle:"+calculatorservice.calculateArea());
+		PolygonPerimeterCalculatorService pericalculatorservice = new Rectangle(3,7);
+		System.out.println("\nArea of rectangle:"+calculatorservice.calculateArea());
+		System.out.println("\nPerimeter of Rectangle:"+pericalculatorservice.caculatePerimeter());
 	}
 	public void testPolygon() {
-		PolygonAreaCalculatorService calculatorservice = new RegularPolygon(3,8); 
-		System.out.println("Area of polygon:"+Math.round(calculatorservice.calculateArea()));
+		PolygonAreaCalculatorService calculatorservice = new RegularPolygon(3,8);
+		PolygonPerimeterCalculatorService pericalculatorservice = new RegularPolygon(3,8);
+		System.out.println("\nArea of polygon:"+Math.round(calculatorservice.calculateArea()));
+		System.out.println("\nPerimeter of Polygon:"+pericalculatorservice.caculatePerimeter());
 	}
 	
 		
