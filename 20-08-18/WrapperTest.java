9ca2c9796b1b70efa83558e41b75483bfa7eeca0
madhavmanohars <madhav.ms@inapp.com>
2018-08-20 09:41:23 +0530

    Tested sin()

--- 20-08-18/WrapperTest.java
+++ 20-08-18/WrapperTest.java
@@ -5,6 +5,9 @@ public static void main(String[] args) {
 	Integer x=10; //boxing
 	x=x+5;        //unboxing
 	System.out.println(x);
+	double degrees = 30.0;
+	double radians = Math.toRadians(degrees);
+	System.out.println(Math.sin(radians));
 }
 }
 
