0f9423b41b29ee24576fbe62e85cff768efe584c
madhavmanohars <madhav.ms@inapp.com>
2018-08-21 15:16:33 +0530

    removed unused variables and methods

--- 20-08-18/Interface_Program/com.inapp.Firstjava.learn/Rectangle.java
+++ 20-08-18/Interface_Program/com.inapp.Firstjava.learn/Rectangle.java
@@ -6,35 +6,20 @@ import com.inapp.Firstjava.learn.inter_face.Polygon;
 public class Rectangle extends Polygon {
  private double length;
  private double breadth;
- private double area;
- private double perimeter;
+
  private boolean square;
 
 public Rectangle()
 {System.out.println("Constructor Rectangle()");
 	}
 public Rectangle(double length,double breadth)
-{   int i;
+{   
 	this.length=length;
 	this.breadth=breadth;
 	double[] sides= {length,breadth,length,breadth};
 	setSides(sides);
 
-//	perimeter=2*(length+breadth);
-/*	while(i<2) {
-		perimeter=length+breadth;
-		i++;
-	}*/
-	/*do
-	{
-		perimeter=length+breadth;
-		i++;
-	}
-	while(i<2);*/
-	for(i=0;i<2;i++) {
-		perimeter=length+breadth;
-	}
-//	System.out.println("Constructor Rectangle(l,b)");
+
 }
 
 public double getLength() {
@@ -42,7 +27,7 @@ public double getLength() {
 }
 public void setLength(double length) {
 	this.length = length;
-	updateAreaAndPerimeter();
+	
 	isSquare();
 }
 public double getBreadth() {
@@ -50,22 +35,13 @@ public double getBreadth() {
 }
 public void setBreadth(double breadth) {
 	this.breadth = breadth;
-	updateAreaAndPerimeter();
+	
 	isSquare();
     
     
 }
-public double getArea() {
-	return area;
-}
 
-public double getPerimeter() {
-	return perimeter;
-}
-private void updateAreaAndPerimeter() {
-	area=length*breadth;
-    perimeter=2*(length+breadth);
-}
+
 public boolean isSquare()
 {   square = breadth == length;
 	return square;
