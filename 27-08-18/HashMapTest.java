6ef4a452ce42a890aab4a9e36648635a902148c1
Madhav Manohar S <madhav.ms@inapp.com>
2018-08-28 15:20:49 +0530

    Update HashMapTest.java

--- 27-08-18/HashMapTest.java
+++ 27-08-18/HashMapTest.java
@@ -1,15 +1,13 @@
 package com.inapp.Firstjava.test;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class HashMapTest {
 	HashMap<Character,String> map = new HashMap<Character,String>();
 public static void main(String[] args) {
 	HashMapTest mapTest = new HashMapTest();
 	mapTest.hashMap();
-	mapTest.getValue();
-	mapTest.removeValue();
-	mapTest.getValue();
 	
 }
 public void hashMap() {
@@ -24,22 +22,10 @@ public void hashMap() {
 	map.put('H',"HEN");
 	map.put('I',"INK");
 	map.put('J',"JUG");
-	System.out.println("\nElements of map before removal: "+map+"\n");
-	System.out.println("Hash before removal: "+map.hashCode());
-        
-}
-public void getValue() {
-	Character key = 'B';
-	if(map.containsKey(key)) 
-	System.out.println("The value for the key"+" "+key+" "+"is "+map.get(key)+"\n");
-	else
-	System.out.println("The value for the key"+" "+key+" "+"is not found \n");
-}
-public void removeValue() {
-	Character key = 'B';
-	map.remove(key);
-	System.out.println("Elements of map after removal: "+map+"\n");
-	System.out.println("Hash after removal: "+map.hashCode());
+	Set<Character> keys= map.keySet();
+	for(char key : keys)
+		System.out.println("The value for key "+key+" is "+map.get(key));
+	
 }
 
 }
