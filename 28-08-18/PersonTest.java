f3c95651cb9f2709f3537881191fdac2d4f94845
madhavmanohars <madhav.ms@inapp.com>
2018-08-28 17:21:42 +0530

    PersonTest

---
+++ 28-08-18/PersonTest.java
@@ -0,0 +1,15 @@
+package com.inapp.Firstjava.test;
+
+public class PersonTest {
+private String Name;
+private String Email;
+private long telephone;
+
+public PersonTest(String Name,String Email,long telephone) {
+	this.Name = Name;
+	this.Email = Email;
+	this.telephone = telephone;
+}
+
+}
+
