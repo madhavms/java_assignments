0f7016419638a3795462fa567904c410db82b500
madhavmanohars <madhav.ms@inapp.com>
2018-08-29 13:35:48 +0530

    Final

--- 28-08-18/test/PersonTest.java
+++ 28-08-18/test/PersonTest.java
@@ -15,8 +15,25 @@ public static void main(String[] args) {
 	arrayList.add(new Person("Darren","darren@tmail.com",719350987));
 	arrayList.add(new Person("Quill","quill@tmail.com",785401987));
 	arrayList.add(new Person("Peter","peter@tmail.com",775643077));
+	Iterator<Person> personiteratorb = arrayList.iterator();
+	System.out.println("before sort\n");
+	while (personiteratorb.hasNext()) {
+		Person personname = personiteratorb.next();
+		System.out.println("Person is"+personname);
+		
+	}
 	PersonSorter personsorter = new PersonSorter(arrayList); 
-	System.out.println(personsorter.getSortedPersonByName());
+	//System.out.println(personsorter.getSortedPersonByName());
+	ArrayList<Person> sortedList = personsorter.getSortedPersonByName();
+	
+	Iterator<Person> personiterator = sortedList.iterator();
+	System.out.println("\nafter sort\n");
+	while (personiterator.hasNext()) {
+		Person personname = personiterator.next();
+		System.out.println("Person is"+personname);
+		
+	}
+	
 }
 
       
