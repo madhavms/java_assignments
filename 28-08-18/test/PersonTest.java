9442217297e9f4eb3ec68c8788d9f1106bdad7f0
madhavmanohars <madhav.ms@inapp.com>
2018-08-29 12:24:10 +0530

    Added persontest

---
+++ 28-08-18/test/PersonTest.java
@@ -0,0 +1,25 @@
+package com.inapp.Firstjava.test;
+import com.inapp.Firstjava.learn.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+public class PersonTest {
+	
+public static void main(String[] args) {
+	ArrayList<Person> arrayList = new ArrayList<Person>();
+	arrayList.add(new Person("Tony","tony@tmail.com",790660987));
+	arrayList.add(new Person("Alan","alan@tmail.com",735640987));
+	arrayList.add(new Person("Darren","darren@tmail.com",719350987));
+	arrayList.add(new Person("Quill","quill@tmail.com",785401987));
+	arrayList.add(new Person("Peter","peter@tmail.com",775643077));
+	PersonSorter personsorter = new PersonSorter(arrayList); 
+	System.out.println(personsorter.getSortedPersonByName());
+}
+
+      
+ 
+}
+
