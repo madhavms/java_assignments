209b877b571900ae04c2b7adcf08cc5ed867a5e9
Madhav Manohar S <madhav.ms@inapp.com>
2018-08-30 23:07:53 +0530

    Update Person.java

--- 29-08-18/Learn/Person.java
+++ 29-08-18/Learn/Person.java
@@ -1,12 +1,6 @@
 package com.inapp.Firstjava.learn;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 
 public class Person {
 	
@@ -14,57 +8,17 @@ public class Person {
 	private String email;
 	private long telephone;
 	static ArrayList<Person> arrayList = new ArrayList<Person>();
-	ArrayList<Person> person = new ArrayList<>();
 	
+	
+	public ArrayList<Person> getArrayList() {
+		return arrayList;
+	}
+
 	public Person() {
 		
 	}
 	
-	public void createPerson(String Name,String Email,long Telephone) {
-		
-		arrayList.add(new Person(Name,Email,Telephone));
-		
-	}    
-	public ArrayList<Person> getPersonByName() throws IOException {
-		ArrayList<Person> personByName = new ArrayList<>();
-		System.out.println("\nEnter the Name: ");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String S = reader.readLine();
-		for(Person personObject:arrayList) {
-			
-			if(personObject.toString().toLowerCase().contains(S.toLowerCase())) {
-				
-				personByName.add(personObject);
-			}
-		}
-		return personByName;
-	}
-	public void printPerson() {
-	
-		
-		Person personsorter = new Person(arrayList); 
-		//System.out.println(personsorter.getSortedPersonByName());
-		ArrayList<Person> sortedList = personsorter.getSortedPersonByName();
-		
-		Iterator<Person> personiterator = sortedList.iterator();
-		System.out.println(" \nSorted Namewise\n");
-		while (personiterator.hasNext()) {
-			Person persondetails = personiterator.next();
-			System.out.println(persondetails);
-			
-		}
-		
-	}
-public Person(ArrayList<Person> person) {         
-	    this.person = person;     
-	  } 
-  
-  public ArrayList<Person> getSortedPersonByName() {         
-    Collections.sort(person, Person.nameComparator);         
-    return person;     
-  } 
-	
-	public String getName() {
+   public String getName() {
 		return name;
 	}
 
